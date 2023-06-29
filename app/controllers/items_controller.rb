849ee3082aa33b49b5c74564203741f957d66f75
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-06-30 00:17:43 +0300

    Almost complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  before_action :find_user
   def index
-    items = Item.all
+    user = User.find(params[:user_id])
+    items = user.items
+    # items = Item.all  
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item.to_json
+  end
+
+  def create
+    item =Item.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+  def render_not_found
+    render json: {error: "Items not found"}, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def find_user
+    user = User.find(params[:user_id])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  def index 
+    user = User.all
+    render json: user
+  end
+  
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  private
+  def render_not_found
+    render json: {error: "User not found"}, status: :not_found
   end
 
 end
